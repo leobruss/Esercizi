905161249ee5e155da8395b65905462cdb8a50a6
leobruss <l.brussani@gmail.com>
2024-05-09 12:52:05 +0200

    aggiunta esercizi

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -3,22 +3,22 @@ class Person:
         '''
         
         '''
-        self._name: str = name
-        self._surname: str = surname
+        self.__name: str = name
+        self.__surname: str = surname
         self.birth_date: str = birth_date
         self.birth_place: str = birth_place
         self.gender: str = gender
-        self._ssn: str = self.compute_ssn()
-    def get_ssn(self) -> None:
+        self.__ssn: str = self.compute__ssn()
+    def get__ssn(self) -> None:
         '''
         This function returns the ssn  the ssn name
         input: none
         return: self._ssn str, this function returns  the ssn value
         '''
 
-        return self._ssn
+        return self.__ssn
     
-    def set_ssn(self, ssn: str) -> None:
+    def set__ssn(self, ssn: str) -> None:
         '''
         This function returns the ssn  the ssn
         input: self._ssn str, this parametere contains the user's snn
@@ -26,55 +26,129 @@ class Person:
         '''
         self._snn = ssn
     
-    def get_name(self):
+    def get__name(self):
         '''
         This function returns a person's name
         input: none
         return: self._name: str, this function returns person's name 
         '''
-        return self._name
+        return self.__name
     
-    def set_name(self, name:str) -> None:
+    def set__name(self, name:str) -> None:
         '''
         This function set the attribute name
         input: name: str, the parameter contains the user's name
         Return: None
         '''
-        self._name = name
-        self._ssn = self.compute_ssn
+        self.__name = name
+        self.__ssn = self.compute__ssn
     
-    def get_surname(self):
+    def get__surname(self):
         '''
         This function returns a person's name
         input: none
         return: self._name: str, this function returns person's name 
         '''
-        return self._surname
+        return self.__surname
     
-    def set_surname(self, surname:str) -> None:
+    def set__surname(self, surname:str) -> None:
         '''
         This function set the attribute name
         input: name: str, the parameter contains the user's name
         Return: None
         '''
         raise Exception("You cannot modify the surname!")
-    def compute_ssn(self) -> str:
+    def compute__ssn(self) -> str:
         '''
         Check the ssn's correction
         '''
 
-        first_three_name_char = self._name[:3]
-        last_three_surname_char = self._surname[-3:]
-        return first_three_name_char + last_three_surname_char
+        first_three_name_char = self.__name
+        vocali: str = "AEIOU"
+        consonanti:str = "BCDFGHJKLMNPQRSTVWXYZ"
+        counter1 = 0
+        counter2 = 0
+        consonanti_accettate_nome: str = ""
+        #calcolo le prime 3 consonanti del nome
+        for char in first_three_name_char.upper():
+            if char in consonanti:
+                counter1 += 1
+                consonanti_accettate_nome += char
+        if len(consonanti_accettate_nome) == 3 :
+            consonanti_accettate_nome = consonanti_accettate_nome
+        if len(consonanti_accettate_nome) > 3 :
+            consonanti_accettate_nome = consonanti_accettate_nome[0] + consonanti_accettate_nome[2] + consonanti_accettate_nome[3]
+        consonanti_mancanti = 3 - len(consonanti_accettate_nome)
+        #se le consonanti sono minori di 3 pasa alle vocali 
+        for char in first_three_name_char.upper():
+            if char in vocali:
+                counter2 += 1
+                if counter2 <= consonanti_mancanti:
+                    consonanti_accettate_nome += char
+        lettere_mancanti = 3 - len(consonanti_accettate_nome)
+        #se anche le vocali non sono abbastanza aggiunge le x
+        for i in range(0, lettere_mancanti):
+            consonanti_accettate_nome += "X"
+        
 
-person_1 : Person = Person(name="Leonardo", surname="Brussasni", birth_date="24/07/2004", birth_place="Rome", gender="Male")
+
+        first_three_surname_char = self.__surname
+        vocali: str = "AEIOU"
+        consonanti:str = "BCDFGHJKLMNPQRSTVWXYZ"
+        counter1 = 0
+        counter2 = 0
+        consonanti_accettate_cognome: str = ""
+        #calcolo le prime 3 consonanti del cognome
+        for char in first_three_surname_char.upper():
+            if char in consonanti:
+                counter1 += 1
+                consonanti_accettate_cognome += char
+        if len(consonanti_accettate_cognome) == 3 :
+            consonanti_accettate_cognome = consonanti_accettate_cognome
+        if len(consonanti_accettate_cognome) > 3 :
+            consonanti_accettate_cognome =  consonanti_accettate_cognome[0] + consonanti_accettate_cognome[1] + consonanti_accettate_cognome[2]
+        consonanti_mancanti = 3 - len(consonanti_accettate_cognome)
+         #se le consonanti sono minori di 3 pasa alle vocali
+        for char in first_three_surname_char.upper():
+            if char in vocali:
+                counter2 += 1
+                if counter2 <= consonanti_mancanti:
+                    consonanti_accettate_cognome += char
+        lettere_mancanti = 3 - len(consonanti_accettate_cognome)
+        #se anche le vocali non sono abbastanza aggiunge le x
+        for i in range(0, lettere_mancanti):
+            consonanti_accettate_cognome += "X"
+
+        #ora calcolo on base al mese di nascita la lettera che va messa dopo le ultime 2 cifre dell'anno di nascita
+        birth_month= self.birth_date[3:5]
+        month: dict = {
+            "01" : "A",  
+            "02" : "B",
+            "03" : "B",
+            "04" : "D",
+            "05" : "E",
+            "06" : "H",
+            "07" : "L",
+            "08" : "M",
+            "09" : "P",
+            "10" : "R",
+            "11" : "S",
+            "12" : "T"
+        }
+        month_letter: str = month[birth_month]
+       
+                
+
+        return consonanti_accettate_cognome + consonanti_accettate_nome + self.birth_date[-2:] + month_letter
+       
+person_1 : Person = Person(name="Leonardo", surname="Brussani", birth_date="24/07/2004", birth_place="Rome", gender="Male")
 
 
 '''queue: list[Person] = [person_1, person_2]
 for person in queue:
-    print(person.get_ssn())'''
+    print(person.get__ssn())'''
 
-print(person_1.get_name())
-print(person_1.get_surname())
-print(person_1.get_ssn())
-print(person_1._name, person_1._surname, person_1._ssn)
+print(person_1.get__name())
+print(person_1.get__surname())
+print(person_1.get__ssn())
+#print(person_1.__name, person_1.__surname, person_1.__ssn)
---
+++ Lezione6/lezione6_aggiuntivi.py
@@ -0,0 +1,73 @@
+class Animal:
+    def __init__(self, name: str, legs:int) ->None:
+        self.name = name
+        self.legs = legs
+    def get_legs(self) -> None:
+        return self.legs 
+    
+    def set_legs(self, legs: str) ->str:
+        self.legs = legs
+
+    def print_info(self) ->None:
+        print(f"The animal {self.name} has {self.legs}")
+
+animal1: Animal = Animal("Lion", 4)
+animal2: Animal = Animal("Fish", 0)
+print(animal1.name)
+print(animal2.name)
+animal1.legs = 4
+animal2.set_legs(0)
+animal1.print_info()
+animal2.print_info()
+print("\n")
+
+
+class Food:
+    def __init__(self, name: str, price: str, description: str) -> None:
+        self.name = name
+        self.price = price
+        self.description = description
+
+class Menu:
+    def __init__(self, foods=None):
+        if foods is None:
+            self.foods = []
+        else:
+            self.foods = foods
+    
+    def addFood(self, food):
+        self.foods.append(food)
+    
+    def removeFood(self, food):
+        self.foods.remove(food)
+    
+    def printPrices(self):
+        print("Menu:")
+        for food in self.foods:
+            print(f"{food.name}: ${food.price}")
+    
+    def getAveragePrice(self):
+        if not self.foods:
+            return 0
+        total_price = sum(food.price for food in self.foods)
+        return total_price / len(self.foods)
+
+food1 = Food("Pizza", 10, "Delicious Italian pizza")
+food2 = Food("Burger", 8, "Classic beef burger")
+food3 = Food("Sushi", 15, "Japanese sushi rolls")
+
+menu = Menu()
+menu.addFood(food1)
+menu.addFood(food2)
+menu.addFood(food3)     
+
+food4 = Food("Pasta", 12, "Homemade spaghetti")
+food5 = Food("Salad", 7, "Fresh garden salad")
+
+menu.addFood(food4)
+menu.addFood(food5)
+
+menu.printPrices()
+
+average_price = menu.getAveragePrice()
+print("Average price of the menu:", average_price)
