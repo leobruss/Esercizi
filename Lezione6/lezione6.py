012ca42233f3b05a985e35a7f58faef5cd131c4d
leobruss <l.brussani@gmail.com>
2024-05-09 13:47:56 +0200

    aggiunta es 6

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -1,8 +1,8 @@
 class Person:
     def __init__(self, name: str, surname: str, birth_date: str, birth_place: str, gender: str) -> None:
-        '''
+        """
         
-        '''
+        """
         self.__name: str = name
         self.__surname: str = surname
         self.birth_date: str = birth_date
@@ -10,58 +10,58 @@ class Person:
         self.gender: str = gender
         self.__ssn: str = self.compute__ssn()
     def get__ssn(self) -> None:
-        '''
+        """
         This function returns the ssn  the ssn name
         input: none
         return: self._ssn str, this function returns  the ssn value
-        '''
+        """
 
         return self.__ssn
     
     def set__ssn(self, ssn: str) -> None:
-        '''
+        """
         This function returns the ssn  the ssn
-        input: self._ssn str, this parametere contains the user's snn
+        input: self._ssn str, this parametere contains the user"s snn
         return: none
-        '''
+        """
         self._snn = ssn
     
     def get__name(self):
-        '''
-        This function returns a person's name
+        """
+        This function returns a person"s name
         input: none
-        return: self._name: str, this function returns person's name 
-        '''
+        return: self._name: str, this function returns person"s name 
+        """
         return self.__name
     
     def set__name(self, name:str) -> None:
-        '''
+        """
         This function set the attribute name
-        input: name: str, the parameter contains the user's name
+        input: name: str, the parameter contains the user"s name
         Return: None
-        '''
+        """
         self.__name = name
         self.__ssn = self.compute__ssn
     
     def get__surname(self):
-        '''
-        This function returns a person's name
+        """
+        This function returns a person"s name
         input: none
-        return: self._name: str, this function returns person's name 
-        '''
+        return: self._name: str, this function returns person"s name 
+        """
         return self.__surname
     
     def set__surname(self, surname:str) -> None:
-        '''
+        """
         This function set the attribute name
-        input: name: str, the parameter contains the user's name
+        input: name: str, the parameter contains the user"s name
         Return: None
-        '''
+        """
         raise Exception("You cannot modify the surname!")
     def compute__ssn(self) -> str:
-        '''
-        Check the ssn's correction
-        '''
+        """
+        Check the ssn"s correction
+        """
 
         first_three_name_char = self.__name
         vocali: str = "AEIOU"
@@ -119,8 +119,8 @@ class Person:
         for i in range(0, lettere_mancanti):
             consonanti_accettate_cognome += "X"
 
-        #ora calcolo on base al mese di nascita la lettera che va messa dopo le ultime 2 cifre dell'anno di nascita
-        birth_month= self.birth_date[3:5]
+        #ora calcolo on base al mese di nascita la lettera che va messa dopo le ultime 2 cifre dell"anno di nascita
+        birth_month: str = self.birth_date[3:5]
         month: dict = {
             "01" : "A",  
             "02" : "B",
@@ -136,17 +136,142 @@ class Person:
             "12" : "T"
         }
         month_letter: str = month[birth_month]
+
+        gender = self.gender
+        birth_day:str = self.birth_date[0:2]
+        if gender.upper() == "F":
+            birth_day = int(birth_day) + 40
+            birth_day =  str(birth_day)
+
+        birth_place = self.birth_place
+        cadastral_codes: dict = {"Ancona":"A271","Aosta":"A326","L'Aquila":"A345","Roma":"H501",
+                                     "Bologna":"A944","Cagliari":"B354","Campobasso":"B519",
+                                     "Catanzaro":"C352","Firenze":"D612","Genova":"D969",
+                                     "Milano":"F205","Napoli":"F839","Palermo":"G273","Bari":"A662",
+                                     "Perugia":"G478","Potenza":"G942","Torino":"L219",
+                                     "Trento":"L378","Trieste":"L424","Venezia":"L736"}  
+        birth_place= cadastral_codes[birth_place]    
+
+        even_nums: dict = {
+            " ": " ",
+            "A": 0,
+            "B": 1,
+            "C": 2,
+            "D": 3,
+            "E": 4,
+            "F": 5,
+            "G": 6,
+            "H": 7,
+            "I": 8,
+            "J": 9,
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "K": 10,
+            "L": 11,
+            "M": 12,
+            "N": 13,
+            "O": 14,
+            "P": 15,
+            "Q": 16,
+            "R": 17,
+            "S": 18,
+            "T": 19,
+            "U": 20,
+            "V": 21,
+            "W": 22,
+            "X": 23,
+            "Y": 24,
+            "Z": 25
+        }
+
+        odd_nums = {
+            " ": " ",
+            "A": 1,
+            "B": 0,
+            "C": 5,
+            "D": 7,
+            "E": 9,
+            "F": 13,
+            "G": 15,
+            "H": 17,
+            "I": 19,
+            "J": 21,
+            "0": 1,
+            "1": 0,
+            "2": 5,
+            "3": 7,
+            "4": 9,
+            "5": 13,
+            "6": 15,
+            "7": 17,
+            "8": 19,
+            "9": 21,
+            "K": 2,
+            "L": 4,
+            "M": 18,
+            "N": 20,
+            "O": 11,
+            "P": 3,
+            "Q": 6,
+            "R": 8,
+            "S": 12,
+            "T": 14,
+            "U": 16,
+            "V": 10,
+            "W": 22,
+            "X": 25,
+            "Y": 24,
+            "Z": 23
+        }
+
+        control_digits: dict = {
+            0: "A",
+            1: "B",
+            2: "C",
+            3: "D",
+            4: "E",
+            5: "F",
+            6: "G",
+            7: "H",
+            8: "I",
+            9: "J",
+            10: "K",
+            11: "L",
+            12: "M",
+            13: "N",
+            14: "O",
+            15: "P",
+            16: "Q",
+            17: "R",
+            18: "S",
+            19: "T",
+            20: "U",
+            21: "V",
+            22: "W",
+            23: "X",
+            24: "Y",
+            25: "Z"
+        }
+
+        codice_di_controllo = ""
+        for i in consonanti_accettate_cognome
+                odd_nums[consonanti_accettate_cognome]
+        return consonanti_accettate_cognome + consonanti_accettate_nome + self.birth_date[-2:] + month_letter + birth_day + birth_place
        
-                
-
-        return consonanti_accettate_cognome + consonanti_accettate_nome + self.birth_date[-2:] + month_letter
-       
-person_1 : Person = Person(name="Leonardo", surname="Brussani", birth_date="24/07/2004", birth_place="Rome", gender="Male")
+person_1 : Person = Person(name="Leonardo", surname="Brussani", birth_date="24/07/2004", birth_place="Roma", gender="M")
 
 
-'''queue: list[Person] = [person_1, person_2]
+"""queue: list[Person] = [person_1, person_2]
 for person in queue:
-    print(person.get__ssn())'''
+    print(person.get__ssn())"""
 
 print(person_1.get__name())
 print(person_1.get__surname())
