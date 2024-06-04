9f41f415140f2c0237d25f184a482589519f5c24
leobruss <l.brussani@gmail.com>
2024-06-04 13:04:58 +0200

    test anagramma

--- Lezione9/Es_Anagramma/test_anagramma.py
+++ Lezione9/Es_Anagramma/test_anagramma.py
@@ -6,26 +6,14 @@ class TestAnagram(unittest.TestCase):
         with open(r'C:\Users\Cloud\Desktop\vscodeproject\Esercizi\Lezione9\Es_Anagramma\anagramma1.txt', 'r') as reader:
             reader.readlines()
 
-if __name__ == '__main__':
-    unittest.main()
-
-class TestAnagram(unittest.TestCase):
     def test_anagram2(self):
         with open(r'C:\Users\Cloud\Desktop\vscodeproject\Esercizi\Lezione9\Es_Anagramma\anagramma2.txt', 'r') as reader:
             reader.readlines()
 
-if __name__ == '__main__':
-    unittest.main()
-
-class TestAnagram(unittest.TestCase):
     def test_anagram3(self):
         with open(r'C:\Users\Cloud\Desktop\vscodeproject\Esercizi\Lezione9\Es_Anagramma\anagramma3.txt', 'r') as reader:
             reader.readlines()
 
-if __name__ == '__main__':
-    unittest.main()
-
-class TestAnagram(unittest.TestCase):
     def test_anagram4(self):
         with open(r'C:\Users\Cloud\Desktop\vscodeproject\Esercizi\Lezione9\Es_Anagramma\anagramma4.txt', 'r') as reader:
             reader.readlines()
