3261a7f91dde2da7f1486da8572071bd576d1be8
leobruss <l.brussani@gmail.com>
2024-06-10 22:25:07 +0200

    aggiunta test

--- Lezione17/test_persona.py
+++ Lezione17/test_persona.py
@@ -53,42 +53,26 @@ class TestPaziente(unittest.TestCase):
         self.assertEqual(self.paziente.getidCode(), '1234567890')
     
 class TestFattura(unittest.TestCase):
-    paziente1 = Paziente("Leonardo", "Brussani", "LB24004")
-    paziente2 = Paziente("Matteo", "Cardelli", "MC24004")
-    paziente3 = Paziente("Lorenzo", "Brussani", "LB09097")
-
     def setUp(self):
-        self.dottore = Dottore('Mario', 'Rossi', 'Ostetrico', 150.0)
-        self.dottore.setAge(45)
-        self.fattura = Fattura([(self.paziente1, self.paziente2, self.paziente3)], self.dottore)
+        self.doctor = Dottore("Giovanni", "Bianchi", "Pediatra", 150.0)
+        self.doctor.setAge(40)
+        self.patients = [Paziente("Luca", "Verdi", "ID12345"), Paziente("Anna", "Neri", "ID67890")]
+        self.fattura = Fattura(self.patients, self.doctor)
 
     def test_initialization(self):
         self.assertEqual(self.fattura.getFatture(), 2)
-        self.assertEqual(self.fattura.getSalary(), 450)
-
-    def test_add_remove_paziente(self):
-
-        self.fattura.addPatient(self.paziente1)
-        self.assertEqual(len(self.fattura._patient), 1)
-        self.assertIn(self.paziente1, self.fattura._patient)
-
-        self.fattura.addPatient(self.paziente2)
-        self.assertEqual(len(self.fattura._patient), 2)
-        self.assertIn(self.paziente2, self.fattura._patient)
-
-        self.fattura.removePatient(self.paziente1)
-        self.assertEqual(len(self.fattura._patient), 1)
-        self.assertNotIn(self.paziente1, self.fattura._patient)
-
-        self.fattura.removePatient(self.paziente2)
-        self.assertEqual(len(self.fattura._patient), 0)
-        self.assertNotIn(self.paziente2, self.fattura._patient)
-
-    def test_salary_calculation(self):
-        self.fattura.addPatient(Paziente('Carlo', 'Verdi', '0987654321'))
-        self.fattura.addPatient(Paziente('Giovanni', 'Neri', '5678901234'))
         self.assertEqual(self.fattura.getSalary(), 300.0)
-        self.assertEqual(self.fattura.getFatture, 2)
+
+    def test_add_patient(self):
+        new_patient = Paziente("Mario", "Rossi", "ID11223")
+        self.fattura.addPatient(new_patient)
+        self.assertEqual(self.fattura.getFatture(), 3)
+        self.assertEqual(self.fattura.getSalary(), 450.0)
+
+    def test_remove_patient(self):
+        self.fattura.removePatient("ID12345")
+        self.assertEqual(self.fattura.getFatture(), 2)
+        self.assertEqual(self.fattura.getSalary(), 300.0)
 
 
 
