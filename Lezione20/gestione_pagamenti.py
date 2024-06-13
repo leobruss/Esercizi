a00987b466020ed2d1a582a1ac52e5145a8e5593
leobruss <l.brussani@gmail.com>
2024-06-13 10:20:55 +0200

    esercizi aggiustati

--- Lezione20/gestione_pagamenti.py
+++ Lezione20/gestione_pagamenti.py
@@ -1,26 +1,34 @@
+# Definizione della classe base 'Pagamento'
 class Pagamento:
     def __init__(self) -> None:
-        self._pagamento: float = None
+        # Attributo privato per memorizzare l'importo del pagamento
+        self._pagamento: float = None  
 
-    def set_pagamento(self, valore: float) ->None:
-        self._pagamento = valore
+    def set_pagamento(self, valore: float) -> None:
+        # Metodo per impostare l'importo del pagamento
 
-    def get_pagamento(self) ->float:
-        return self._pagamento
-
-    def dettagliPagamento(self) ->str:
-        return f"Importo del pagamento: €{self._pagamento}"  
-
-
-class PagamentoContanti (Pagamento):
-    def __init__(self) -> None:
-        super().__init__()
+        self._pagamento = valore  
+    def get_pagamento(self) -> float:
+        # Metodo per ottenere l'importo del pagamento
+        return self._pagamento  
 
     def dettagliPagamento(self) -> str:
-        print(f"Pagamento in contanti di: €{self._pagamento}")
-        self.inPezziDa()
+        # Metodo per ottenere una stringa con i dettagli del pagamento
+        return f"Importo del pagamento: €{self._pagamento}"  
 
-    def inPezziDa(self) ->str:
+# Definizione della classe 'PagamentoContanti' che eredita da 'Pagamento'
+class PagamentoContanti(Pagamento):
+    def __init__(self) -> None:
+        super().__init__()  # Chiama il costruttore della classe base
+
+    def dettagliPagamento(self) -> str:
+        # Stampa i dettagli del pagamento in contanti
+        print(f"Pagamento in contanti di: €{self._pagamento}")  
+        # Chiama il metodo per calcolare e stampare la divisione in pezzi
+        self.inPezziDa()  
+
+    # Dizionario con i tagli delle banconote e monete in euro
+    def inPezziDa(self) -> str:
         valori_euro = {
             '500€': 500,
             '200€': 200,
@@ -36,51 +44,87 @@ class PagamentoContanti (Pagamento):
             '0,10€': 0.10,
             '0,05€': 0.05,
             '0,01€': 0.01
-        } 
+        }  
 
-        importo_restante = self.get_pagamento()
-        pezzi_banconote = {}
-        for pezzi, valori in valori_euro.items():
+        # Ottiene l'importo del pagamento
+        importo_restante = self.get_pagamento()  
+
+        # Dizionario per memorizzare il numero di pezzi di ogni taglio
+        pezzi_banconote = {}  
+
+         # Itera sui tagli delle banconote e monete
+        for pezzi, valori in valori_euro.items(): 
+             # Calcola il numero di pezzi per ogni taglio
             numero_pezzi = int(importo_restante // valori)
-            if importo_restante > 0 and numero_pezzi > 0:
-                pezzi_banconote[pezzi] = numero_pezzi
-                print(f"{numero_pezzi} banconote da {pezzi}")
-            importo_restante = round(importo_restante - numero_pezzi * valori, 2)
-            
-    
 
+            if importo_restante > 0 and numero_pezzi > 0:
+                # Memorizza il numero di pezzi
+                pezzi_banconote[pezzi] = numero_pezzi  
+                
+                # Stampa il numero di pezzi
+                print(f"{numero_pezzi} banconote da {pezzi}")  
+
+                # Aggiorna l'importo restante
+            importo_restante = round(importo_restante - numero_pezzi * valori, 2) 
+
+# Definizione della classe 'PagamentoCartaDiCredito' che eredita da 'Pagamento'
 class PagamentoCartaDiCredito(Pagamento):
     def __init__(self, nome: str, data_scadenza: str, numero_di_carta: int) -> None:
-        super().__init__()
-        self.nome = nome
-        self.data_scadenza = data_scadenza
-        self.numero_di_carta = numero_di_carta
-
-
+        super().__init__()  # Chiama il costruttore della classe base
+         # Inizializza il nome del titolare della carta
+        self.nome = nome 
+        # Inizializza la data di scadenza della carta
+        self.data_scadenza = data_scadenza  
+        # Inizializza il numero della carta
+        self.numero_di_carta = numero_di_carta  
 
     def dettagliPagamento(self) -> str:
-        print(f"Pagamento di: €{self.get_pagamento()} effettuato con la carta di credito\nNome sulla carta: {self.nome}\nData di scadenza: {self.data_scadenza}\nNumero della carta: {self.numero_di_carta}")
-    
+        print(f"Pagamento di: €{self.get_pagamento()} effettuato con la carta di credito\nNome sulla carta: {self.nome}\nData di scadenza: {self.data_scadenza}\nNumero della carta: {self.numero_di_carta}")  # Stampa i dettagli del pagamento con carta di credito
 
-
-
-
-
+# Creazione di un'istanza della classe 'PagamentoContanti'
 pagamento_contanti1: PagamentoContanti = PagamentoContanti()
-pagamento_contanti1.set_pagamento(95.25)
-pagamento_contanti1.dettagliPagamento()
+
+ # Imposta l'importo del pagamento
+pagamento_contanti1.set_pagamento(95.25) 
+
+# Stampa i dettagli del pagamento e la divisione in pezzi
+pagamento_contanti1.dettagliPagamento()  
+
 print()
 
+# Creazione di un'altra istanza della classe 'PagamentoContanti'
 pagamento_contanti2: PagamentoContanti = PagamentoContanti()
-pagamento_contanti2.set_pagamento(215.35)
-pagamento_contanti2.dettagliPagamento()
+
+# Imposta l'importo del pagamento
+pagamento_contanti2.set_pagamento(215.35)  
+
+ # Stampa i dettagli del pagamento e la divisione in pezzi
+pagamento_contanti2.dettagliPagamento() 
+
+print()
+print("--*--" * 36)
 print()
 
+# Creazione di un'istanza della classe 'PagamentoCartaDiCredito'
 pagamento_carta1: PagamentoCartaDiCredito = PagamentoCartaDiCredito("Leonardo Brussani", "24/07", 2365489514782365)
-pagamento_carta1.set_pagamento(450)
-pagamento_carta1.dettagliPagamento()
+
+# Imposta l'importo del pagamento
+pagamento_carta1.set_pagamento(450)  
+
+# Stampa i dettagli del pagamento con carta di credito
+pagamento_carta1.dettagliPagamento()  
+
+print()
+print("--*--" * 36)
 print()
 
+# Creazione di un'altra istanza della classe 'PagamentoCartaDiCredito'
 pagamento_carta2: PagamentoCartaDiCredito = PagamentoCartaDiCredito("Matteo Cardelli", "10/03", 14725642314865749)
-pagamento_carta2.set_pagamento(50)
-pagamento_carta2.dettagliPagamento()
+
+# Imposta l'importo del pagamento
+pagamento_carta2.set_pagamento(50)  
+
+# Stampa i dettagli del pagamento con carta di credito
+pagamento_carta2.dettagliPagamento()  
+
+ 
--- Lezione20/rendering_grafico.py
+++ Lezione20/rendering_grafico.py
@@ -1,93 +1,92 @@
 from abc import ABC
+
+# Definizione della classe base Forma che estende ABC (Abstract Base Class)
 class Forma(ABC):
-        def __init__(self, nome: str) -> None:
-                self.nome = nome
+    def __init__(self, nome: str) -> None:
+        self.nome = nome
 
-        def getArea(self) ->None:
-                pass
-        
-        def render(self) ->None:
-                pass
-        
+    def getArea(self) -> None:
+        pass  # Metodo astratto, da implementare nelle classi derivate
+
+    def render(self) -> None:
+        pass  # Metodo astratto, da implementare nelle classi derivate
+
+# Classe Quadrato che estende Forma
 class Quadrato(Forma):
-        def __init__(self, nome: str, lunghezza_lato: int) -> None:
-                super().__init__(nome)
-                self.lunghezza_lato = lunghezza_lato
-                print(f"Il valore del lato del {nome} è pari a {lunghezza_lato}")
+    def __init__(self, nome: str, lunghezza_lato: int) -> None:
+        super().__init__(nome)
+        self.lunghezza_lato = lunghezza_lato
+        print(f"Il valore del lato del {nome} è pari a {lunghezza_lato}")
 
-        def getArea(self) -> None:
-                self.area_quadrato = self.lunghezza_lato ** 2
-                print(F"L'area del {self.nome} è pari a {self.area_quadrato}")
+    def getArea(self) -> None:
+        self.area_quadrato = self.lunghezza_lato ** 2
+        print(F"L'area del {self.nome} è pari a {self.area_quadrato}")
 
+    def render(self) -> None:
+        # Costruzione della rappresentazione del quadrato con asterischi
+        lato_lungo: str = "* " * self.lunghezza_lato
+        spazio_vuoto: str = "* " + "  " * (self.lunghezza_lato - 2) + "*\n"
+        linee_spazi_vuoti: str = spazio_vuoto * (self.lunghezza_lato - 2)
+        if self.lunghezza_lato > 0:
+            print(f"{lato_lungo}\n{linee_spazi_vuoti}{lato_lungo}")
 
-        def render(self) -> None:
-
-            lato_lungo: str = "* " * self.lunghezza_lato
-            spazio_vuoto: str = "* " + "  " * (self.lunghezza_lato - 2) + "*\n"
-            linee_spazi_vuoti: str = spazio_vuoto * (self.lunghezza_lato - 2) 
-            if self.lunghezza_lato > 0:
-                print(f"{lato_lungo}\n{linee_spazi_vuoti}{lato_lungo}")
-
-
+# Classe Rettangolo che estende Forma
 class Rettangolo(Forma):
-        def __init__(self, nome: str, base: int, altezza: int) -> None:
-                self.nome = nome    
-                self.altezza = altezza 
-                self.base = base
-                print(f"Il valore dei lati del {nome} sono pari a:\nBase: {base}\nAltezza: {altezza}")
+    def __init__(self, nome: str, base: int, altezza: int) -> None:
+        self.nome = nome
+        self.altezza = altezza
+        self.base = base
+        print(f"Il valore dei lati del {nome} sono pari a:\nBase: {base}\nAltezza: {altezza}")
 
-        def getArea(self) -> None:
-                self.area_rettangolo = self.altezza * self.base 
-                print(F"L'area del {self.nome} è pari a {self.area_rettangolo}")
+    def getArea(self) -> None:
+        self.area_rettangolo = self.altezza * self.base
+        print(F"L'area del {self.nome} è pari a {self.area_rettangolo}")
 
-        def render(self) -> None:
-                lato_lungo: str = "* " * self.base
-                spazio_vuoto: str = "* " + "  " * (self.base - 2) + "*\n"
-                linee_spazi_vuoti: str = spazio_vuoto * (self.altezza - 2) 
-                
-                print(f"{lato_lungo}\n{linee_spazi_vuoti}{lato_lungo}")
+    def render(self) -> None:
+        # Costruzione della rappresentazione del rettangolo con asterischi
+        lato_lungo: str = "* " * self.base
+        spazio_vuoto: str = "* " + "  " * (self.base - 2) + "*\n"
+        linee_spazi_vuoti: str = spazio_vuoto * (self.altezza - 2)
+        print(f"{lato_lungo}\n{linee_spazi_vuoti}{lato_lungo}")
 
+# Classe Triangolo che estende Forma
+class Triangolo(Forma):
+    def __init__(self, nome: str, lunghezza_base: int, lunghezza_altezza: int) -> None:
+        super().__init__(nome)
+        self.lunghezza_base = lunghezza_base
+        self.lunghezza_altezza = lunghezza_altezza
 
+    def getArea(self) -> None:
+        self.area_triangolo = (self.lunghezza_base * self.lunghezza_altezza) / 2
+        print(F"L'area del {self.nome} è pari a {self.area_triangolo}")
 
+    def render(self) -> None:
+        # Costruzione della rappresentazione del triangolo con asterischi
+        for a in range(self.lunghezza_altezza):
+            for b in range(self.lunghezza_base):
+                if a == self.lunghezza_altezza - 1 or a == b or b == 0:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
 
-
-
-class Triangolo (Forma):
-        def __init__(self, nome: str, lunghezza_base: int, lunghezza_altezza: int) -> None:
-            super().__init__(nome)               
-            self.lunghezza_base = lunghezza_base
-            self.lunghezza_altezza = lunghezza_altezza
-
-        def getArea(self) -> None:
-            self.area_triangolo = (self.lunghezza_base * self.lunghezza_altezza) / 2
-            
-        def render(self) -> None:
-                for a in range(self.lunghezza_altezza):
-                        for b in range(self.lunghezza_base):
-                                if a == self.lunghezza_altezza -1 or a == b or b== 0:
-                                       print("*", end="")
-                                else:
-                                       print(" ", end="")
-
-                        print()
-                  
-
-
-
-
+# Creazione e test delle istanze
 quadrato1: Quadrato = Quadrato("quadrato", 4)
 quadrato1.getArea()
 quadrato1.render()
 
-print("\n")
+print()
+print("--*--" * 36)
+print()
 
 rettangolo1: Rettangolo = Rettangolo("rettangolo", 8, 4)
 rettangolo1.getArea()
 rettangolo1.render()
 
-print("\n")
+print()
+print("--*--" * 36)
+print()
 
 triangolo1: Triangolo = Triangolo("triangolo", 10, 10)
 triangolo1.getArea()
 triangolo1.render()
-
